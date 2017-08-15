7fe8103f94501f3addf5c015c8802b40b07cad29
supermay <wu_jiamei@outlook.com>
2017-08-15 11:46:27 +0200

    article page wip

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'site#index'
 
-  resources :articles, only: [:index, :create, :destroy, :update]
+  resources :articles, only: [:index, :show, :create, :destroy, :update]
   resources :photos, only: [:index, :create, :destroy, :update]
 
 end
